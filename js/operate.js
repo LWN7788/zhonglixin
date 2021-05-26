043cf8c104c54754b60813aeb3af573fcb6cc2b6
liujinyuan <liujinyuan@sunvua.com>
2021-05-26 10:27:22 +0800

    接口联调

--- js/operate.js
+++ js/operate.js
@@ -1,20 +1,17 @@
 var base_url = 'http://localhost:8128';
-function initbutton(className) {
-    if($('.' + className).length > 0){
-        $('.' + className).show()
-    }else{
-        $('.list-wrapper').append("<div class='tabpane "+className+"'></li>")
-    }
 
-    $('.button-wrappper').on('click', '.button', function () {
-        $('.button-wrappper').children().removeClass('choosed')
-        $(this).addClass('choosed')
-        var className = $(this).attr('code')
-        if($('.' + className).length > 0){
-            $('.' + className).show()
-        }else{
-            $('.list-wrapper').append("<div class='tabpane "+className+"'></li>")
+$(function () {
+    $.ajax({
+        url: base_url + "/QdxData/titleUrlList",
+        dataType: "JSON",
+        cache: false,
+        type: "GET",
+        success: function (res) {
+            var data = {
+                list: res.data
+            }
+            var html = template('titleList', data);
+            $('.tab').html(html);
         }
-        $('.' + className).siblings().hide()
     })
-}
+})
